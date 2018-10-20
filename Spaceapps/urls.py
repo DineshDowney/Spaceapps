2ee575a2eef3f1423732ca3fbb215c3147635557
Dinesh Downey <dinesh.downey@yahoo.com>
2018-10-21 02:42:30 +0530

    we wasted hours on it"

--- Spaceapps/urls.py
+++ Spaceapps/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.index,name="index"),
    # url(r'^page',views.Sample,name="Sample Page"),
+  # url(r'^upload/$',views.upload,name="upload"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
 from PIL import Image,ImageEnhance,ImageChops
 import requests
+import random as rn
 from io import BytesIO
+import os
+from Spaceapps.settings import TEMPLATE_DIR as dry
 # Create your views here.
 def index(request):
-    return render(request,"app.html")
+        global d
+        d = "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/6/"+str(rn.randint(1,39))+"/"+str(rn.randint(1,79))+".jpg"
+        manipulate()
+        #enhance()
+        print(dry+"/filters/")
+        return render(request,"app.html", {'img':d})
 
-def manipulate(url):
-    response = requests.get(url)
+def manipulate():
+    response = requests.get(d)
     img = Image.open(BytesIO(response.content))
     size = img.size
     for y in range(1,5):
-        img2=Image.open("filters/filter ({}).jpg".format(y))
+        img2=Image.open(os.path.join(dry,"filters","filter({}).jpg".format(y)))
         img2=img2.resize(size, Image.ANTIALIAS)
         img3=ImageChops.blend(img,img2,alpha=(4/10))
-        img3=ImageChops.darker()
-        img3.save("outputs/Artified_img-{}.jpg".format(y))
+        img3=ImageChops.darker(img,img2)
+        img3.save(os.path.join(dry,"outputs","Artified_img-{}.jpg".format(y)))
+
  
-def enhance(url):    
-    response = requests.get(url)
+def enhance():    
+    response = requests.get(d)
     img = Image.open(BytesIO(response.content))
     enhancer = ImageEnhance.Color(img)
     factor=1.5
@@ -29,4 +38,4 @@ def enhance(url):
     enhancer = ImageEnhance.Sharpness(img)
     factor=2
     img=enhancer.enhance(factor)
-    img.save("outputs/Enhanced_Image.jpg")
+    img.save("/outputs/Enhanced_Image.jpg")
